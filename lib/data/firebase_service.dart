a0c4db0273ab4796a0325a0f60ddbf806b0cc58d
valenguz <valecarmoguz@gmail.com>
2026-04-28 14:40:56 -0500

    FirebaseService
    Removed comments for clarity in FirebaseService methods.

--- lib/data/firebase_service.dart
+++ lib/data/firebase_service.dart
@@ -2,15 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/libro_model.dart';
 
 class FirebaseService {
-  // Referencia central a la colección
   final CollectionReference _db = FirebaseFirestore.instance.collection('valentina_eritz');
 
-  // CREATE: Guardar un libro nuevo
   Future<void> addLibro(LibroModel libro) async {
     await _db.add(libro.toFirestore());
   }
-
-  // READ: Obtener todos los libros en tiempo real
+  
   Stream<List<LibroModel>> getLibros() {
     return _db.snapshots().map((snapshot) {
       return snapshot.docs.map((doc) {
@@ -19,19 +16,16 @@ class FirebaseService {
     });
   }
 
-  // UPDATE: Actualizar datos de un libro existente
   Future<void> updateLibro(LibroModel libro) async {
     if (libro.id != null) {
       await _db.doc(libro.id!).update(libro.toFirestore());
     }
   }
 
-  // DELETE: Eliminar un libro
   Future<void> deleteLibro(String id) async {
     await _db.doc(id).delete();
   }
 
-  // SEARCH: Buscar por nombre (sensible a mayúsculas)
   Stream<List<LibroModel>> buscarLibro(String nombre) {
     return _db
         .where('Titulo', isGreaterThanOrEqualTo: nombre)
